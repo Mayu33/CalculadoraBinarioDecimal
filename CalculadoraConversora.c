790f6ab71cf77f5c9a34f10daf755503644e6ae0
Mayu33 <58526412+Mayu33@users.noreply.github.com>
2020-04-26 20:16:29 -0300

    Update CalculadoraConversora.c

--- CalculadoraConversora.c
+++ CalculadoraConversora.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>  // Biblioteca para funções getch() e getche()
-#include <string.h> // Funções de manipulação de strings
-#include <ctype.h>  // Função isdigit
-#include <math.h>   // Função POV
-#include <locale.h> // Configurar a acentuação
+#include <conio.h>  // Biblioteca para funÃ§Ãµes getch() e getche()
+#include <string.h> // FunÃ§Ãµes de manipulaÃ§Ã£o de strings
+#include <ctype.h>  // FunÃ§Ã£o isdigit
+#include <math.h>   // FunÃ§Ã£o POV
+#include <locale.h> // Configurar a acentuaÃ§Ã£o
 
-/*                      Calculadora Conversora de Binário e Decimal
+/*                      Calculadora Conversora de BinÃ¡rio e Decimal
 
-                    Faz conversões de Decimal para Binário e Binário para Decimal
+                    Faz conversÃµes de Decimal para BinÃ¡rio e BinÃ¡rio para Decimal
 
     Autoras: Laudiane de O.
              Mayara B. da Silva
@@ -16,31 +16,31 @@
     Data: 26/04/2020
 */
 
-char valor[50];     // Variável que armazena o número digitado pelo usuário
+char valor[50];     // VariÃ¡vel que armazena o nÃºmero digitado pelo usuÃ¡rio
 
 
-// Função para parte escrita do Menu
+// FunÃ§Ã£o para parte escrita do Menu
 void imprimeMenu(){
-     printf ("Calculadora Conversora Binário Decimal\n\n");
-     printf ("Escolha a conversão:\n\n\t");
-     printf ("1 - Decimal     -> Binário\n\t");
-     printf ("2 - Binário     -> Decimal\n\t");
+     printf ("Calculadora Conversora BinÃ¡rio Decimal\n\n");
+     printf ("Escolha a conversÃ£o:\n\n\t");
+     printf ("1 - Decimal     -> BinÃ¡rio\n\t");
+     printf ("2 - BinÃ¡rio     -> Decimal\n\t");
      printf ("0 - Sair\n\n");
 }
 
-// Função principal do Menu
+// FunÃ§Ã£o principal do Menu
 void menu()
 {
-     char opc;              // variável que recebe a opção escolhida pelo usuário
-     imprimeMenu();         // função que imprime a parte escrita menu
-     printf ("Opção: ");
-     opc = getche();        // função que lê a opção digitada pelo usuário sem necessidade de teclar "enter"
+     char opc;              // variÃ¡vel que recebe a opÃ§Ã£o escolhida pelo usuÃ¡rio
+     imprimeMenu();         // funÃ§Ã£o que imprime a parte escrita menu
+     printf ("OpÃ§Ã£o: ");
+     opc = getche();        // funÃ§Ã£o que lÃª a opÃ§Ã£o digitada pelo usuÃ¡rio sem necessidade de teclar "enter"
 
-     switch (opc)           // Função para executar o menu de acordo com a opção digitada pelo usuário
+     switch (opc)           // FunÃ§Ã£o para executar o menu de acordo com a opÃ§Ã£o digitada pelo usuÃ¡rio
      {
             case '1':
-                 DecParaBin();      // Função correspondente ao cálculo desejado
-                 voltaMenu(&opc);   // Função que consulta o usuário para retornar ao menu ou encerrar a calculadora
+                 DecParaBin();      // FunÃ§Ã£o correspondente ao cÃ¡lculo desejado
+                 voltaMenu(&opc);   // FunÃ§Ã£o que consulta o usuÃ¡rio para retornar ao menu ou encerrar a calculadora
                  break;             // Fim do case
             case '2':
                  BinParaDec();
@@ -48,65 +48,65 @@ void menu()
                  break;
             case '0':
                  exit(1);
-            default:                                // Caso o usuário digite alguma opção inválida
+            default:                                // Caso o usuÃ¡rio digite alguma opÃ§Ã£o invÃ¡lida
                     system ("cls");                 // Limpa a tela
                     imprimeMenu();                  // Imprime o menu
-                    printf("Opção inválida.\n");    // Informa que a opção é inválida
-                    system("pause");                // Pede ao usuário para pressionar qualquer tecla para continuar
+                    printf("OpÃ§Ã£o invÃ¡lida.\n");    // Informa que a opÃ§Ã£o Ã© invÃ¡lida
+                    system("pause");                // Pede ao usuÃ¡rio para pressionar qualquer tecla para continuar
                     system ("cls");                 // Limpa a tela
                     menu();                         // Chama o menu novamente
      }
 }
 
-// Função que pergunta ao usuário se deseja retornar ao menu após as conversões.
+// FunÃ§Ã£o que pergunta ao usuÃ¡rio se deseja retornar ao menu apÃ³s as conversÃµes.
 void voltaMenu()
 {
      char op = '0';
-     printf ("\n\nDeseja retornar ao menu? [1] Sim | [2] Não ");
-     op = getche();             // Função que lê a opção digitada pelo usuário sem necessidade de pressionar enter. Essa função só pode ser usada com tipo char
+     printf ("\n\nDeseja retornar ao menu? [1] Sim | [2] NÃ£o ");
+     op = getche();             // FunÃ§Ã£o que lÃª a opÃ§Ã£o digitada pelo usuÃ¡rio sem necessidade de pressionar enter. Essa funÃ§Ã£o sÃ³ pode ser usada com tipo char
 
      if (op=='1'){
         system ("cls");
         menu();
      }else if (op=='2'){
         printf("\nEncerrando...");
-     }else{                     // Caso o usuário digite uma opção inválida, ele irá retornar ao menu após pressionar qualquer tecla.
-        printf("\nOpção inválida. Retornando ao menu...\n");
+     }else{                     // Caso o usuÃ¡rio digite uma opÃ§Ã£o invÃ¡lida, ele irÃ¡ retornar ao menu apÃ³s pressionar qualquer tecla.
+        printf("\nOpÃ§Ã£o invÃ¡lida. Retornando ao menu...\n");
         system("pause");
         system("cls"); // Limpa a tela
         menu();
         }
 }
 
-// Função que converte Decimal para Binário
+// FunÃ§Ã£o que converte Decimal para BinÃ¡rio
 void DecParaBin()
 {
-     char binario[33];      // String em C: vetor da função "char" chamado binário com 33 posições
-     char buffer[33];       //              vetor chamado buffer com 33 posições
+     char binario[33];      // String em C: vetor da funÃ§Ã£o "char" chamado binÃ¡rio com 33 posiÃ§Ãµes
+     char buffer[33];       //              vetor chamado buffer com 33 posiÃ§Ãµes
      char c;
      int divisor = 2;
-     int resto, i, j, quociente, temp = 0;
+     int resto = 0, i = 0, j = 0, quociente = 0, temp = 0;
 
-     printf ("\n\nInforme o número inteiro desejado: ");
+     printf ("\n\nInforme o nÃºmero inteiro desejado: ");
 
-     // Bloco de código abaixo verifica se o número digitado é válido
+     // Bloco de cÃ³digo abaixo verifica se o nÃºmero digitado Ã© vÃ¡lido
 
      do{
-        c=getch();          // captura a tecla digitada pelo usuário
-        if (isdigit(c)!=0){ // Função isdigit verifica se um caractere é um digito decimal, em caso positivo
-          valor[j] = c;     // variável valor recebe o caractere digitado pelo usuário
-          j++;              // a variável de controle é incrementada
-          printf ("%c", c); // exibido na tela o número digitado
-       }else if(c==8&&j){   // condição para saber se a tecla pressionada foi o BACKSPACE e se foram digitados caracteres para serem apagados
-          valor[j]='\0';    // é colocada a terminação "\0" no indice atual do vetor
-          j--;              // a variável de controle é decrementada
-          printf("\b \b");  // o caractere é apagado da tela
+        c=getch();          // captura a tecla digitada pelo usuÃ¡rio
+        if (isdigit(c)!=0){ // FunÃ§Ã£o isdigit verifica se um caractere Ã© um digito decimal, em caso positivo
+          valor[j] = c;     // variÃ¡vel valor recebe o caractere digitado pelo usuÃ¡rio
+          j++;              // a variÃ¡vel de controle Ã© incrementada
+          printf ("%c", c); // exibido na tela o nÃºmero digitado
+       }else if(c==8&&j){   // condiÃ§Ã£o para saber se a tecla pressionada foi o BACKSPACE e se foram digitados caracteres para serem apagados
+          valor[j]='\0';    // Ã© colocada a terminaÃ§Ã£o "\0" no indice atual do vetor
+          j--;              // a variÃ¡vel de controle Ã© decrementada
+          printf("\b \b");  // o caractere Ã© apagado da tela
        }
-     }while(c!=13);         // o loop ocorrerá até que seja pressionado enter
-     valor[j]='\0';         // ao final do loop colocamos a terminação "\0" na posição atual do vetor
+     }while(c!=13);         // o loop ocorrerÃ¡ atÃ© que seja pressionado enter
+     valor[j]='\0';         // ao final do loop colocamos a terminaÃ§Ã£o "\0" na posiÃ§Ã£o atual do vetor
 
-     // Bloco de código abaixo faz a conversão do valor decimal em binário.
-     quociente = atoi(valor);   // função "atoi" converte string em número inteiro. O vetor "valor" é uma variável global;
+     // Bloco de cÃ³digo abaixo faz a conversÃ£o do valor decimal em binÃ¡rio.
+     quociente = atoi(valor);   // funÃ§Ã£o "atoi" converte string em nÃºmero inteiro. O vetor "valor" Ã© uma variÃ¡vel global;
      strcpy(binario, "");       // copia " " para a string binario, ou seja, esvazia ela.
      while (quociente >= divisor)
      {
@@ -114,44 +114,44 @@ void DecParaBin()
            temp = quociente/divisor;
            quociente = temp;
            sprintf (buffer, "%d", resto); // formata uma string e guarda o resultado em um array, funciona igual ao printf, mas salva o resultado em um ponteiro
-           strcat (binario, buffer);      // junta na variável "binário" o conteúdo da variável "binário e buffer"
+           strcat (binario, buffer);      // junta na variÃ¡vel "binÃ¡rio" o conteÃºdo da variÃ¡vel "binÃ¡rio e buffer"
      }
      sprintf (buffer, "%d", quociente);   // assim como dentro do loop while
-     strcat (binario, buffer);            // novamente a função srtcat
+     strcat (binario, buffer);            // novamente a funÃ§Ã£o srtcat
 
-     printf ("\n\nConvertido em binário: %s\n\n", strrev(binario)); // imprime a string invertida
+     printf ("\n\nConvertido em binÃ¡rio: %s\n\n", strrev(binario)); // imprime a string invertida
 }
 
 void BinParaDec()
 {
-     int tamanho, result, temp, i, j = 0;
+     int tamanho = 0, result = 0, temp = 0, i = 0, j = 0;
      char c;
-     printf ("\n\nInforme um valor binário para ser convertido: ");
+     printf ("\n\nInforme um valor binÃ¡rio para ser convertido: ");
 
-     // Bloco de código abaixo verifica se o número digitado é válido
+     // Bloco de cÃ³digo abaixo verifica se o nÃºmero digitado Ã© vÃ¡lido
      do{
        c=getch();
        if (c=='0' || c=='1'){
-          valor[j] = c;         // atribuição do caractere informado a variável "valor"
-          j++;                  // a variável de controle é incrementada
-          printf ("%c", c);     // o valor é impresso na tela
-       }else if(c==8&&j){ // condição para saber se a tecla pressionada foi BACKSPACE e se foram digitados caracteres para que sejam apagados
-          valor[j]='\0';        // é colocada a terminação "\0" no indice atual do vetor
-          j--;                  // a variável de controle é decrementada
-          printf("\b \b");      // o caractere é apagado da tela
+          valor[j] = c;         // atribuiÃ§Ã£o do caractere informado a variÃ¡vel "valor"
+          j++;                  // a variÃ¡vel de controle Ã© incrementada
+          printf ("%c", c);     // o valor Ã© impresso na tela
+       }else if(c==8&&j){ // condiÃ§Ã£o para saber se a tecla pressionada foi BACKSPACE e se foram digitados caracteres para que sejam apagados
+          valor[j]='\0';        // Ã© colocada a terminaÃ§Ã£o "\0" no indice atual do vetor
+          j--;                  // a variÃ¡vel de controle Ã© decrementada
+          printf("\b \b");      // o caractere Ã© apagado da tela
        }
-    }while(c!=13);              // o loop acontecerá até que seja pressionada a tecla enter
+    }while(c!=13);              // o loop acontecerÃ¡ atÃ© que seja pressionada a tecla enter
 
-     valor[j]='\0';             // ao final do loop, colocamos a terminação "\0" na posição do atual vetor
+     valor[j]='\0';             // ao final do loop, colocamos a terminaÃ§Ã£o "\0" na posiÃ§Ã£o do atual vetor
 
-     // Bloco de código abaixo faz a conversão do número binário em decimal
-     tamanho = strlen(valor);           // Função strlen lê o tamanho da string e passa para uma var do tipo int, nessa caso a int tamanho
-     tamanho--;                         // tamanho é decrementado em 1
-     while (tamanho >= 0){              // este loop irá acontecer enquando a variável tamanho for maior ou igual a 0, a cada loop ela é decrementada
+     // Bloco de cÃ³digo abaixo faz a conversÃ£o do nÃºmero binÃ¡rio em decimal
+     tamanho = strlen(valor);           // FunÃ§Ã£o strlen lÃª o tamanho da string e passa para uma var do tipo int, nessa caso a int tamanho
+     tamanho--;                         // tamanho Ã© decrementado em 1
+     while (tamanho >= 0){              // este loop irÃ¡ acontecer enquando a variÃ¡vel tamanho for maior ou igual a 0, a cada loop ela Ã© decrementada
            temp = pow (2, tamanho);                 // a variavel temp recebe 2 no expoente do tamanho da string
-           result += ((valor[i] - 48) * temp);      // result recebe a soma da "string" (valor[i] - 48) * variável temp
-           tamanho--;                               // tamanho é decrementado no loop em execução
-           i++;                                     // e i é incrementado no loop em execução
+           result += ((valor[i] - 48) * temp);      // result recebe a soma da "string" (valor[i] - 48) * variÃ¡vel temp
+           tamanho--;                               // tamanho Ã© decrementado no loop em execuÃ§Ã£o
+           i++;                                     // e i Ã© incrementado no loop em execuÃ§Ã£o
      }
 
      printf ("\n\nConvertido em decimal: %d\n\n", result);
@@ -160,10 +160,10 @@ void BinParaDec()
 
 
 
-// Função Principal da calculadora
+// FunÃ§Ã£o Principal da calculadora
 int main (void)
 {
-    setlocale(LC_ALL,"portuguese"); // Configura a acentuação
+    setlocale(LC_ALL,"portuguese"); // Configura a acentuaÃ§Ã£o
 
     menu();
     printf("\n\n");
